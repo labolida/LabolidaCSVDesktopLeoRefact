6af87a6260f103b4159475c7aa4c95ca3ed89173
Leonardo Labolida <labolida.leonardo@gmail.com>
2022-07-12 20:50:59 +0200

    move from ./layer1/Launcher to root directory

--- Launcher.java
+++ Launcher.java
@@ -1,9 +1,15 @@
-package layer1front;
+
+
+import layer1front.Context;
+import layer1front.GUI;
 
 public class Launcher {
 	
     public static void main( String args[] ) { 
     	try {
+            if (args.length == 1) {         // nome do arquivo opcional
+                Context.fileName = args[0];
+            }
 			GUI gui = new GUI();
 			Context.gui=gui;
 		}
